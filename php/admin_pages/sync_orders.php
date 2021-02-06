0083365549f9b21e86f2ee5bb0f3068b6d3c02bc
manolie <man.olie@yahoo.com>
2021-02-06 09:09:53 +0200

    feature ready

--- php/admin_pages/sync_orders.php
+++ php/admin_pages/sync_orders.php
@@ -13,7 +13,29 @@ if (isset($_REQUEST["init_orders_sync_process"])) {
     $result_html = "";
 }
 
+if ($archcommerce_subscriptionService->is_insert_orders_active()) {
+    $order_statuses = wc_get_order_statuses();
+    unset($order_statuses["wc-cancelled"]);
+    unset($order_statuses["wc-refunded"]);
+    unset($order_statuses["wc-failed"]);
+
+    $order_wp_query_args = array(
+        "post_type" => "shop_order",
+        'post_status' => array_keys($order_statuses),
+        "meta_query" => array(
+            array(
+                "key" => "_archcommerce_soft1_id",
+                "value" => "",
+                "compare" => "NOT EXISTS"
+            )
+        )
+    );
+    $order_wp_query = new \WP_Query($order_wp_query_args);
+    $orders_unsynced = $order_wp_query->found_posts;
+    wp_reset_postdata();
+}
 ?>
+
 <div class="wrap">
     <span class="dashicons dashicons-cart"></span>
     <h1 class="wp-heading-inline"><?php _e("Sync orders", "archcommerce"); ?></h1>
@@ -23,6 +45,7 @@ if (isset($_REQUEST["init_orders_sync_process"])) {
             <input type="hidden" name="init_orders_sync_process" />
             <button class="button-primary"><?php _e("sync orders now", "archcommerce"); ?></button>
         </form>
+        <p> <?php echo sprintf(__('There are <strong>%s</strong> unsynced order(s)', 'archcommerce'), $orders_unsynced); ?> </p>
         <?php echo $result_html; ?>
     <?php else : ?>
         <p><?php _e("Sync orders is not enabled in your subscription", "archcommerce"); ?></p>
